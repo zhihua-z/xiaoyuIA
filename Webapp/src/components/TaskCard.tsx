cf13d858e6c51507fb083fda1f38fc1a0fa2d774
mxy-1326 <mxyy.06@gmail.com>
2024-12-15 20:01:50 +0800

    15Dec

--- Webapp/src/components/TaskCard.tsx
+++ Webapp/src/components/TaskCard.tsx
@@ -1,36 +1,41 @@
+import { useState, useContext } from 'react'
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import CardActionArea from '@mui/material/CardActionArea';
+import { useNavigate } from 'react-router-dom';
+import { ITask } from "../interfaces";
+import useLocalStorage from '../utils/useLocalStorage';
 
-const TaskCard = ({ color, children } : {color: string, children: any}) => {
+
+const TaskCard = ({ item }: {item: ITask}) => {
+    console.log("here",item)
+    const [deadline, setDeadline] = useLocalStorage("deadline", "")
+    const navigate = useNavigate()
+
     return (
-        <Box
+        <Paper
+            square={false}
+            elevation={1}
             sx={{
                 width: '100%',
-                height: 75,
-                margin: 'auto',
-                mt: 3,
-                borderRadius: 10
+                mt: 0.5,
+                borderRadius: 2,
+                overflow: 'hidden'
             }}
         >
-            <Box sx={{
-                height: '100%',
-                width: '100%',
-                display: 'flex',
-                flexDirection: 'column'
-            }}>
-                <Box
-                    sx={{
-                        background: color,
-                        height: '100%',
-                        width: '98%',
-                        margin: 'auto',
-                        borderRadius: 5
-                    }}
-                >
-                    {children}
+            <CardActionArea onClick={() => { navigate('/detail') }}>
+                <Box id={'display_caption'} sx={{ ml: 0.5, mr: 0.5 }}>
+                    <Typography fontSize={16} fontWeight={600}>{item.TaskName}</Typography>
+                    <Box sx={{ flex: 1, display: 'flex', flexDirection: 'row' }} justifyContent={'space-between'}>
+                        <Typography fontSize={12} fontWeight={300} sx={{ mt: 1 }}>{item.TaskCreateTime}</Typography>
+                        <Typography fontSize={12} fontWeight={300} sx={{ mt: 1, ml: 1 }}>{item.TaskDeadline}</Typography>
+
+                    </Box>
                 </Box>
-            </Box>
-        </Box>
+            </CardActionArea>
+        </Paper>
     )
 }
 
-export default TaskCard;
+export default TaskCard
